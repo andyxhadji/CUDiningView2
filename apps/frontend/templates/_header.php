4db1ba2d8f5b3f8fba82d74047432d69147b39cf
Andy Hadjigeorgiou <ahh2131@columbia.edu>
2013-08-10 10:52:15 -0400

    navbar left justified!

--- apps/frontend/templates/_header.php
+++ apps/frontend/templates/_header.php
@@ -1,6 +1,6 @@
     <div class="navbar">
       <div class="navbar-inner">
-        <div class="container offset6" style="text-align:center">
+        <div>
           <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
