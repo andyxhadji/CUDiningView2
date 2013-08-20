6d7b3c9bd5433c5ed4e0d0fedc10492abdc4384e
Andy Hadjigeorgiou <ahh2131@columbia.edu>
2013-08-20 18:37:18 -0400

    changes

---
+++ apps/frontend/modules/myplate/actions/actions.class.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * myplate actions.
+ *
+ * @package    symfony
+ * @subpackage myplate
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class myplateActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+  	$arr = $_SESSION['facebook']->getSignedRequest();
+  	$userId = $arr['user_id'];
+  	$user = UserQuery::create()->filterByUserId($userId)->findOne();
+  	if  ($user):
+  		$_SESSION['foods'] = unserialize($user->getFood());
+  		$_SESSION['myplate'] = array();
+  		$_SESSION['myfoodnow'] = array();
+  		foreach ($_SESSION['foods'] as $food)
+  		{
+  			$plate = NutritionQuery::create()->filterByFOODID($food)->findOne();
+  			$_SESSION['myplate'][] = $plate;
+  			if ($plate->getCURRENT() == 1)
+  			{
+  				$_SESSION['myfoodnow'][] = $plate;
+  			}
+  		}
+  	else:
+  		$_SESSION['myplate'] = array();
+  	    $_SESSION['myfoodnow'] = array();
+    endif;
+  }
+}
---
+++ apps/frontend/modules/myplate/templates/indexSuccess.php
@@ -0,0 +1,2 @@
+<?php include_partial("global/diningview", array('foods' => $_SESSION['myfoodnow'], 'hall' => 'Available Now', 'me' => 1, 'number' => 0)) ?>
+<?php include_partial("global/diningview", array('foods' => $_SESSION['myplate'], 'hall' => 'My Plate', 'me' => 1, 'number' => 1)) ?>
