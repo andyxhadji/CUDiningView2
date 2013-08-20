3feb20d4c68b43fae7416a8a4d7a3057aca16530
Andy Hadjigeorgiou <ahh2131@columbia.edu>
2013-08-20 18:20:26 -0400

    homepage changes

--- apps/frontend/modules/dininghall/templates/_home.php
+++ apps/frontend/modules/dininghall/templates/_home.php
@@ -1,25 +1,33 @@
 <div class="tab-pane active" id="<?php echo $hall ?>">
-  <div class="hero-unit">
-    <p align="center">
-      <img src="/images/logo.png">
-    <div style="text-align:center">
-     <!-- <div class="fb-like" data-href="http://cudiningview.com/" data-send="false" data-width="450" data-show-faces="true" data-font="lucida grande" data-action="recommend"></div>
-   -->
-    </div>
-    <!--
-  Below we include the Login Button social plugin. This button uses the JavaScript SDK to
-  present a graphical Login button that triggers the FB.login() function when clicked.
-
-  Learn more about options for the login button plugin:
-  /docs/reference/plugins/login/ -->
-  <div class="row-fluid">
-    <div class='offset5'>
-
-<fb:login-button show-faces="true" width="200" max-rows="1"></fb:login-button>
-
-    </div>   
-  </div>
-      </p>
-      <br>
-  </div>
+	<div style="text-align: center;background: WhiteSmoke;">
+		  <img src="/images/logo.png"  width="550">
+	</div>
+	<div style="text-align: center;">
+		  <fb:login-button show-faces="true" width="200" max-rows="1" ></fb:login-button>
+		  <hr style="width: 50%;text-align: center;height: 1px;border: 0px;border-color: gainsboro;background-color: gainsboro;color: gainsboro;margin-left: auto; margin-right: auto; visibility: hidden; " align="center">
+	</div>
+	
+	<div class="row-fluid">
+		<div class="span4" style="padding-bottom: 10px;">
+			<div  style="text-align: center; width: 200px; border: 2px solid gray; margin-left: auto;margin-right: auto; box-shadow: 0px 2px 5px #888888; padding-bottom: 10px;" align="center">
+			<p style="background: whitesmoke; font-size: 30px; font-weight: bold; height: 32px; padding-top: 8px; box-shadow: 0px 1px 1px #888888;">John Jay</p>	
+			<p style="font-size: 25px; padding-bottom: 5px;">Busy</p>
+			<a href="/johnjay" class="btn btn-primary">More Info</a>
+			</div>
+		</div>
+		<div class="span4" style="padding-bottom: 10px;">
+			<div  style="text-align: center; width: 200px; border: 2px solid gray; margin-left: auto;margin-right: auto; box-shadow: 0px 2px 5px #888888; padding-bottom: 10px;" align="center">
+			<p style="background: whitesmoke; font-size: 30px; font-weight: bold; height: 32px; padding-top: 8px; box-shadow: 0px 1px 1px #888888;">Ferris</p>
+			<p style="font-size: 25px; padding-bottom: 5px;">Empty</p>
+			<a href="/johnjay" class="btn btn-primary">More Info</a>
+			</div>
+		</div>
+		<div class="span4" style="padding-bottom: 10px;">
+			<div  style="text-align: center; width: 200px; border: 2px solid gray; margin-left: auto;margin-right: auto; box-shadow: 0px 2px 5px #888888; padding-bottom: 10px;" align="center">
+			<p style="background: whitesmoke; font-size: 30px; font-weight: bold; height: 32px; padding-top: 8px; box-shadow: 0px 1px 1px #888888;">JJs</p>
+			<p style="font-size: 25px; padding-bottom: 5px;">Closed</p>
+			<a href="/johnjay" class="btn btn-primary">More Info</a>
+			</div>
+		</div>
+	</div>
 </div>
