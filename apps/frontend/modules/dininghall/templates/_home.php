08404334c2d7a0ef55d0bb477c8ceb5f89580ca3
Andy Hadjigeorgiou <ahh2131@columbia.edu>
2013-08-24 10:53:39 -0400

    new homepage yey

--- apps/frontend/modules/dininghall/templates/_home.php
+++ apps/frontend/modules/dininghall/templates/_home.php
@@ -1,12 +1,21 @@
 <div class="tab-pane active" id="<?php echo $hall ?>">
-	<div style="text-align: center;background: WhiteSmoke;">
+	<div style="background: WhiteSmoke;"> 
+	<div style="text-align: center; padding-bottom: 10px;">
 		  <img src="/images/logo.png"  width="550">
 	</div>
-	<div style="text-align: center;">
-		  <fb:login-button show-faces="true" width="200" max-rows="1" ></fb:login-button>
-		  <hr style="width: 50%;text-align: center;height: 1px;border: 0px;border-color: gainsboro;background-color: gainsboro;color: gainsboro;margin-left: auto; margin-right: auto; visibility: hidden; " align="center">
+	<div style="height: 10px;"></div>
+	<div class="row-fluid hide_mobile">
+		  <div class="offset3 span6">
+		  <p style="font-size: 18px; font-family: Trebuchet MS; text-align: center;"> 
+		  <span  style="font-size: 22px; font-weight: bold;">We think meals are the most important parts of the day. </span>
+		  This site is meant to help you make informed decisions about what you eat and avoid dining hall congestion while doing it.
+		  Vote for and subscribe to different foods, check nutrition facts, or geek out on our <a href="#">detailed statistics</a> page. 
+		  Let's make a better dining hall experience for everyone.</p>
+		  </div>
+		  <div class="span3" style="text-align: center; margin-left: auto; margin-right: auto; padding-bottom: 10px;"><fb:login-button show-faces="true" width="200" max-rows="1" ></fb:login-button></div>
 	</div>
-	
+	</div>
+	<div style="height: 20px;"></div>
 	<div class="row-fluid">
 		<div class="span4" style="padding-bottom: 10px;">
 			<div  style="text-align: center; width: 200px; border: 2px solid gray; margin-left: auto;margin-right: auto; box-shadow: 0px 2px 5px #888888; padding-bottom: 10px;" align="center">
--- apps/frontend/templates/_footer.php
+++ apps/frontend/templates/_footer.php
@@ -1,5 +1,6 @@
   <div id="footer">
       <div class="container">
+		<hr style="width: 80%; height: 1px;border: 0px;border-color: gainsboro;background-color: gainsboro;color: gainsboro; margin-left: auto; margin-right: auto;">
         <div class="row-fluid">
         <p class="muted credit" style="text-align: center;">Created by Sam Aronson, Sandeep "Sunny" Balasubramanian, Parker Case, and Andy Hadjigeorgiou.</p>
       </div>
