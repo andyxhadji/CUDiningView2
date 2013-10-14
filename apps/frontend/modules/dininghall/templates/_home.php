f79920f9517015b0e71736ddd406ef8c80f0b36b
Andy Hadjigeorgiou <ahh2131@columbia.edu>
2013-10-14 10:42:49 -0400

    updated analytics

--- apps/frontend/modules/dininghall/templates/_home.php
+++ apps/frontend/modules/dininghall/templates/_home.php
@@ -1,3 +1,16 @@
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-38330335-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 <div class="tab-pane active" id="<?php echo $hall ?>">
 	<div class="banner"> 
 	<div style="text-align: center; padding-bottom: 10px;">
--- apps/frontend/templates/_footer.php
+++ apps/frontend/templates/_footer.php
@@ -2,7 +2,7 @@
       <div class="container">
 		<hr style="width: 80%; height: 1px;border: 0px;border-color: gainsboro;color: gainsboro; margin-left: auto; margin-right: auto;">
         <div class="row-fluid">
-        <p class="muted credit" style="text-align: center;">Created by Sam, <a href="http://www.sandeepba.com">Sunny</a>, <a href="http://www.prkrcs.com">Parker</a>, and Andy.</p>
+        <p class="muted credit" style="text-align: center;">Created by Sam, <a href="http://www.sandeepba.com">Sunny</a>, <a href="http://www.prkrcs.com">Parker</a>, and <a href="http://www.sandeepba.com">Sunny</a>, <a href="http://www.github.com/ahh2131">Andy</a>.</p>
       </div>
       </div>
     </div>
